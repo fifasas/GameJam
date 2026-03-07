6a374e95821869764ecef788cdd162dca5d21a75
Risastindl <risastindl@seznam.cz>
2026-03-07 13:30:52 +0100

    polishing

--- scripts/player/character_body_2d.gd
+++ scripts/player/character_body_2d.gd
@@ -1,32 +1,38 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -600.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Přidání gravitace
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Skok
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Získání směru pohybu
 	var direction := Input.get_axis("left", "right")
 	
+	# Otočení postavy podle směru
 	if direction > 0:
-		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.flip_h = true # Opraveno: většinou je vpravo false
 	elif direction < 0:
-		animated_sprite_2d.flip_h  = false
+		animated_sprite_2d.flip_h = false
 	
-		
+	# Přepínání animací
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("Idle")
+		else:
+			animated_sprite_2d.play("walk")
+	else: # Tady byla ta chyba v odsazení (byla tu mezera navíc)
+		animated_sprite_2d.play("jump") # Opraven překlep z .plya na .play
 	
-	
+	# Výpočet rychlosti
 	if direction:
 		velocity.x = direction * SPEED
 	else:
