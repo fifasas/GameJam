a69a1820bf5fe8ea5379d79edddde3f642603ce4
fifasas <filipek.lnenicka@gmail.com>
2026-03-08 11:59:23 +0100

    sounds, deadScreen

--- scripts/healing.gd
+++ scripts/healing.gd
@@ -1,17 +1,19 @@
 extends Area2D
 
-# Kolik HP má tento předmět doplnit (můžeš měnit v Inspektoru)
 @export var heal_amount: int = 20
+@onready var fish_audio: AudioStreamPlayer2D = $FishAudio
+
+func _ready() -> void:
+	if fish_audio.stream != null:
+		fish_audio.play()
 
 func _on_body_entered(body: Node2D) -> void:
-	# Zkontrolujeme, zda to, co do předmětu narazilo, je hráč
-	# a zda má metodu "heal" (kterou ve svém kódu máš)
 	if body.has_method("heal"):
-		# Pokud je hráč už plně vyhealovaný, volitelně můžeš 
-		# přidat podmínku, aby se předmět nespotřeboval:
-		# if body.current_hp < body.max_hp:
-		
 		body.heal(heal_amount)
-		
-		# Předmět zmizí ze světa
 		queue_free()
+
+func _on_fish_audio_finished() -> void:
+	if is_queued_for_deletion():
+		return
+	if fish_audio.stream != null:
+		fish_audio.play()
